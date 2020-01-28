9544071cd0549b3c7c9eebdbd9e868cbf4cb3525
UO264254 <UO264254@uniovi.es>
2020-01-28 12:31:59 +0100

    Añadiendo linea Nuevo cambio en Instancia STS 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo Git");
 		System.out.println("Primer cambio");
+		System.out.println("Nuevo cambio Instancia 1");
 	}
 
 }
